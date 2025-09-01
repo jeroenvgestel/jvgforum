dd2f1069a35cc4af2a694a4e00bbfc583ea228a8
Jeroen van Gestel <jeroen@vangestel.dev>
2025-09-01 18:30:08 +0200

    Initial commit

---
+++ cache/forum_index_cache.php
@@ -0,0 +1,219 @@
+<?php
+return array (
+  3 => 
+  array (
+    'index' => 3,
+    'type' => 0,
+    'parent_index' => -1,
+    'name' => 'Announcements',
+    'desc' => '',
+    'topic_count' => 0,
+    'post_count' => 0,
+    'lastpost' => 
+    array (
+      'user' => 
+      array (
+        'name' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/usercp/-1',
+        'style' => 'color:red;',
+        'avatarUrl' => NULL,
+        'avatarBgColor' => 'style="fill: " ',
+        'prefix' => NULL,
+        'suffix' => NULL,
+      ),
+      'topic' => 
+      array (
+        'index' => -1,
+        'title' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/topic/-1',
+      ),
+      'date' => 'Never',
+      'timestamp' => 0,
+    ),
+    'parent' => NULL,
+    'children' => 
+    array (
+    ),
+    'url' => 'http://127.0.0.1/jvgforum/forum/3',
+  ),
+  4 => 
+  array (
+    'index' => 4,
+    'type' => 1,
+    'parent_index' => 3,
+    'name' => 'News',
+    'desc' => 'All the latest new available here',
+    'topic_count' => 0,
+    'post_count' => 0,
+    'lastpost' => 
+    array (
+      'user' => 
+      array (
+        'name' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/usercp/-1',
+        'style' => 'color:red;',
+        'avatarUrl' => NULL,
+        'avatarBgColor' => 'style="fill: " ',
+        'prefix' => NULL,
+        'suffix' => NULL,
+      ),
+      'topic' => 
+      array (
+        'index' => -1,
+        'title' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/topic/-1',
+      ),
+      'date' => 'Never',
+      'timestamp' => 0,
+    ),
+    'parent' => NULL,
+    'children' => 
+    array (
+    ),
+    'url' => 'http://127.0.0.1/jvgforum/forum/4',
+  ),
+  5 => 
+  array (
+    'index' => 5,
+    'type' => 1,
+    'parent_index' => 3,
+    'name' => 'Events',
+    'desc' => 'Information about our current and past Events',
+    'topic_count' => 0,
+    'post_count' => 0,
+    'lastpost' => 
+    array (
+      'user' => 
+      array (
+        'name' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/usercp/-1',
+        'style' => 'color:red;',
+        'avatarUrl' => NULL,
+        'avatarBgColor' => 'style="fill: " ',
+        'prefix' => NULL,
+        'suffix' => NULL,
+      ),
+      'topic' => 
+      array (
+        'index' => -1,
+        'title' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/topic/-1',
+      ),
+      'date' => 'Never',
+      'timestamp' => 0,
+    ),
+    'parent' => NULL,
+    'children' => 
+    array (
+    ),
+    'url' => 'http://127.0.0.1/jvgforum/forum/5',
+  ),
+  6 => 
+  array (
+    'index' => 6,
+    'type' => 0,
+    'parent_index' => -1,
+    'name' => 'General',
+    'desc' => '',
+    'topic_count' => 0,
+    'post_count' => 0,
+    'lastpost' => 
+    array (
+      'user' => 
+      array (
+        'name' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/usercp/-1',
+        'style' => 'color:red;',
+        'avatarUrl' => NULL,
+        'avatarBgColor' => 'style="fill: " ',
+        'prefix' => NULL,
+        'suffix' => NULL,
+      ),
+      'topic' => 
+      array (
+        'index' => -1,
+        'title' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/topic/-1',
+      ),
+      'date' => 'Never',
+      'timestamp' => 0,
+    ),
+    'parent' => NULL,
+    'children' => 
+    array (
+    ),
+    'url' => 'http://127.0.0.1/jvgforum/forum/6',
+  ),
+  7 => 
+  array (
+    'index' => 7,
+    'type' => 1,
+    'parent_index' => 6,
+    'name' => 'General Discussion',
+    'desc' => 'Anything game related can be posted here',
+    'topic_count' => 0,
+    'post_count' => 0,
+    'lastpost' => 
+    array (
+      'user' => 
+      array (
+        'name' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/usercp/-1',
+        'style' => 'color:red;',
+        'avatarUrl' => NULL,
+        'avatarBgColor' => 'style="fill: " ',
+        'prefix' => NULL,
+        'suffix' => NULL,
+      ),
+      'topic' => 
+      array (
+        'index' => -1,
+        'title' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/topic/-1',
+      ),
+      'date' => 'Never',
+      'timestamp' => 0,
+    ),
+    'parent' => NULL,
+    'children' => 
+    array (
+    ),
+    'url' => 'http://127.0.0.1/jvgforum/forum/7',
+  ),
+  9 => 
+  array (
+    'index' => 9,
+    'type' => 1,
+    'parent_index' => 6,
+    'name' => 'Technical Support',
+    'desc' => 'Need help with a problem?',
+    'topic_count' => 0,
+    'post_count' => 0,
+    'lastpost' => 
+    array (
+      'user' => 
+      array (
+        'name' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/usercp/-1',
+        'style' => 'color:red;',
+        'avatarUrl' => NULL,
+        'avatarBgColor' => 'style="fill: " ',
+        'prefix' => NULL,
+        'suffix' => NULL,
+      ),
+      'topic' => 
+      array (
+        'index' => -1,
+        'title' => NULL,
+        'url' => 'http://127.0.0.1/jvgforum/topic/-1',
+      ),
+      'date' => 'Never',
+      'timestamp' => 0,
+    ),
+    'parent' => NULL,
+    'children' => 
+    array (
+    ),
+    'url' => 'http://127.0.0.1/jvgforum/forum/9',
+  ),
+);
---
+++ cache/forum_permissions_cache.php
@@ -0,0 +1,213 @@
+<?php
+return array (
+  3 => 
+  array (
+    2 => 
+    array (
+      0 => true,
+      1 => false,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    3 => 
+    array (
+      0 => true,
+      1 => false,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    4 => 
+    array (
+      0 => true,
+      1 => false,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    6 => 
+    array (
+      0 => true,
+      1 => false,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+  ),
+  4 => 
+  array (
+    2 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    3 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => false,
+      3 => true,
+      4 => false,
+    ),
+    4 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => true,
+    ),
+    6 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => true,
+    ),
+  ),
+  5 => 
+  array (
+    2 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    3 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => false,
+      3 => true,
+      4 => false,
+    ),
+    4 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => true,
+    ),
+    6 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => true,
+    ),
+  ),
+  6 => 
+  array (
+    2 => 
+    array (
+      0 => true,
+      1 => false,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    3 => 
+    array (
+      0 => true,
+      1 => false,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    4 => 
+    array (
+      0 => true,
+      1 => false,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    6 => 
+    array (
+      0 => true,
+      1 => false,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+  ),
+  7 => 
+  array (
+    2 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    3 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => false,
+    ),
+    4 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => true,
+    ),
+    6 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => true,
+    ),
+  ),
+  9 => 
+  array (
+    2 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => false,
+      3 => false,
+      4 => false,
+    ),
+    3 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => false,
+    ),
+    4 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => true,
+    ),
+    6 => 
+    array (
+      0 => true,
+      1 => true,
+      2 => true,
+      3 => true,
+      4 => true,
+    ),
+  ),
+);
